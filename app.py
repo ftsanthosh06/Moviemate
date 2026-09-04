463c3f2d135898500280348e8f1e08c3189821ce
ftsanthosh06 <ft.santhosh06@gmail.com>
2026-09-04 19:08:53 +0530

    Add production URL to allowed origins

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def create_app():
             "http://127.0.0.1:5174",
             "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "https://moviemate-self.vercel.app",
         ],
     )
 
